fdb253b20ffa6edce7f14e9929d5033f21b5e058
Devang Ajagiya <devangajagiya@gmail.com>
2024-09-26 20:54:16 -0400

    add loops

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-console.log( 'Hello, world!' );
-alert( 'Succesfully' );
+// console.log( 'Hello, world!' );
+// alert( 'Succesfully' );
 
-let a=50, b=20;
-console.log( a+b, a-b, a*b, a/b );
+// let a=50, b=20;
+// console.log( a+b, a-b, a*b, a/b );
 
-function hello(a,b){
-  return a+b;
+// function hello(a,b){
+//   return a+b;
+// }
+// console.log( hello(a,b) );
+
+let x = 0;
+while (x < 10) {
+  document.write(x); 
+  x++;
 }
-console.log( hello(a,b) );
-
-document.body.innerHTML = "<h1>Hello there</h1>";
+ 
