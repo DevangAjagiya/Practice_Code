2fd677de50a3297720da1aa284b21f0583d5ca10
Devang Ajagiya <devangajagiya@gmail.com>
2024-09-26 21:08:39 -0400

    updated

--- index.js
+++ index.js
@@ -9,9 +9,26 @@
 // }
 // console.log( hello(a,b) );
 
-let x = 0;
-while (x < 10) {
-  document.write(x); 
-  x++;
-}
+// let x = 0;
+// while (x < 10) {
+//   document.write(x); 
+//   x++;
+// }
  
+// Functions
+
+function selected(selectObject){
+  let numObj = 1;
+  for (let i = 0; i < selectObject.options.length; i++){
+    if (selectObject.options[i].selected){
+      numObj++;
+    }
+  }
+  return numObj;
+}
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function(){
+  alert('Hello, world!');
+});
